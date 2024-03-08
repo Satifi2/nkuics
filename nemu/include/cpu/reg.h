b45eb240357fe93d4b2434db30f2d948b8252a51
tracer-ics2017 <tracer@njuics.org>
2024-03-08 06:52:32 -0800

    >  compile 161220000 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  06:52:31 up  2:12,  1 user,  load average: 0.23, 0.30, 0.28 2c99df946e152f36a23bc5412ad0180f78ea09d

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,18 +15,27 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+
+  union  {
+    struct {
+      uint32_t _32;
+      uint16_t _16;
+      uint8_t _8[2];
+    } gpr[8];
+
+    struct
+    {
+      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    };
+
+  };
 
   /* Do NOT change the order of the GPRs' definitions. */
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+
 
   vaddr_t eip;
 
@@ -50,10 +59,10 @@ extern const char* regsb[];
 static inline const char* reg_name(int index, int width) {
   assert(index >= 0 && index < 8);
   switch (width) {
-    case 4: return regsl[index];
-    case 1: return regsb[index];
-    case 2: return regsw[index];
-    default: assert(0);
+  case 4: return regsl[index];
+  case 1: return regsb[index];
+  case 2: return regsw[index];
+  default: assert(0);
   }
 }
 
