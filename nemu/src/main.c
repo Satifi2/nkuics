72c396edcb8a84e9ad4dbd3b53b0fbfbbf310eb2
tracer-ics2017 <tracer@njuics.org>
2024-03-09 23:45:31 -0800

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  23:45:31 up 10:56,  1 user,  load average: 0.20, 0.05, 0.01 ccebe1d3cd9f3f36288b518bffde1253b2de92fd

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,24 +1,6 @@
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
-
-
-#include <stdio.h>
-#include <string.h>
-union Data{
-  int i;
-  float f;
-  char str[20];
-};
-
-void testunion(){
-  union Data data;
-  data.i=10;
-  printf("data.i: %d\n",data.i);
-  data.f=220.5;
-  printf("data.f:%f\n",data.f);
-  strcpy(data.str,"Test Union");
-  printf("data.s:%s\n",data.str);
-}
+void testunion();
 
 int main(int argc, char *argv[]) {
   testunion();
