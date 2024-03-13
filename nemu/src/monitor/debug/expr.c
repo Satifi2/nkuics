f27c6c51920841b7d55bff87270a3704d9fbaf31
tracer-ics2017 <tracer@njuics.org>
2024-03-12 21:15:05 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  21:15:05 up 1 day, 17:08,  1 user,  load average: 0.21, 0.08, 0.02 8f193459fdf01c07eeea2544e353828354e5b9d7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -200,7 +200,7 @@ bool check_parentheses(int s, int e) {
 
 int op_priority(int type) {
   switch (type) {
-  case TK_OR: return 10;
+  case TK_OR: return 11;
   case TK_AND: return 10;
   case TK_EQ: return 20;
   case TK_NOTEQUAL: return 20;
