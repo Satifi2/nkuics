dc9e7dd3d22e0eb736a7844cd508734001af1fe2
tracer-ics2017 <tracer@njuics.org>
2024-03-13 05:22:45 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  05:22:45 up 2 days, 12 min,  1 user,  load average: 0.00, 0.00, 0.00 91a79bcbbd21e57bf7d758f5186b1f8db6556a04

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -21,3 +21,46 @@ void init_wp_pool() {
 /* TODO: Implement the functionality of watchpoint */
 
 
+WP* new_wp(char *expr, int val) {
+  if (free_ == NULL) {
+    printf("Exception: No free watchpoints available.\n");
+    return NULL;
+  }
+  
+  WP* wp = free_; // 从空闲列表中获取一个监视点
+  free_ = free_->next; // 更新空闲列表的头指针
+  
+  // 初始化监视点
+  strncpy(wp->expr, expr, 512); // 复制表达式
+  wp->value = val; // 设置值
+  wp->next = NULL; // 新监视点将被加到链表尾部，所以next为NULL
+  
+  // 将监视点加入到活动链表中
+  if (head == NULL) {
+    // 如果活动链表为空，新监视点即为头节点
+    head = wp;
+  } else {
+    // 否则，遍历到链表尾部，并将新监视点加入
+    WP* last = head;
+    while (last->next != NULL) {
+      last = last->next;
+    }
+    last->next = wp;
+  }
+  
+  return wp; // 返回新创建的监视点
+}
+
+void print_wp() {
+  if (head == NULL) {
+    printf("No watchpoints.\n");
+    return;
+  }
+  
+  printf("NO\texp\n");
+  WP* wp = head;
+  while (wp != NULL) {
+    printf("%d\t%s\n", wp->NO, wp->expr);
+    wp = wp->next;
+  }
+}
