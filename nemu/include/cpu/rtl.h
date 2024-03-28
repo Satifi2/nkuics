3b9016907e865a04825a37f95a8d5cb13e5b861c
tracer-ics2017 <tracer@njuics.org>
2024-03-27 23:44:43 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  23:44:42 up 3 days, 21:57,  1 user,  load average: 0.22, 0.09, 0.02 1062e73634e0648ab2816a42ed7a4ae8fbe8f1ec

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -143,28 +143,30 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  TODO();
+  cpu.esp-=4;
+  vaddr_write(cpu.esp,4,*src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  TODO();
+  *dest=vaddr_read(cpu.esp,4);
+  cpu.esp+=4;
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  *dest=(*src1==0?1:0);
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-  TODO();
+  *dest = (*src1 == imm ? 1 : 0);
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-  TODO();
+  *dest = ( *src1 != 0 ? 1 :0);
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
