3af57e1c7baad1d2e4345c4ccc63b1caf51c646b
tracer-ics2017 <tracer@njuics.org>
2024-03-27 20:25:51 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:25:51 up 3 days, 18:38,  1 user,  load average: 0.00, 0.00, 0.00 f7548b03e1a798bc10ce19bdf7c94942afcf89df

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -4,7 +4,23 @@ void sscanfTest();
 void strtokTest();
 void test_regex();
 void powTest() ;
+#include <stdio.h>
+
+#define DEFINE_PRINT_FUNCTION(type, specifier) \
+  void print_##type(type value) { \
+    printf(#specifier "\n", value); \
+  }
+
+// 使用宏定义来生成具体的打印函数
+DEFINE_PRINT_FUNCTION(int, %d)
+DEFINE_PRINT_FUNCTION(float, %f)
+DEFINE_PRINT_FUNCTION(char, %c)
 int main(int argc, char* argv[]) {
+
+  print_int(123);
+  print_float(456.789);
+  print_char('A');
+
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
