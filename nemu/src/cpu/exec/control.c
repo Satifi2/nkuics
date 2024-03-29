7318810d5ad280e3ad5254bcf5b6d217eb3ff12b
tracer-ics2017 <tracer@njuics.org>
2024-03-28 19:32:34 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  19:32:34 up 4 days,  8:29,  1 user,  load average: 0.00, 0.00, 0.00 f32b3104e1258170e91685813e1ddf0d6b9cb242

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -27,7 +27,6 @@ make_EHelper(call) {
   // the target address is calculated at the decode stage
   decoding.is_jmp=1;
   rtl_push(&decoding.seq_eip);
-
   print_asm("call %x", decoding.jmp_eip);
 }
 
