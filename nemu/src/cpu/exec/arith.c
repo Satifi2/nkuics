6b379e8ca0a603ad662026ced1b339f275fa39d0
tracer-ics2017 <tracer@njuics.org>
2024-03-30 00:59:01 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  00:59:00 up 4 days, 21:37,  1 user,  load average: 0.07, 0.03, 0.01 8f97be30136b3dd29a073bc801e55cd5edac739

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,15 +7,20 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
+  //// 将源操作数和目标操作数的值符号扩展到32位,存放在t1和t2
   rtl_sext(&t1, &id_dest->val, id_dest->width);
   rtl_sext(&t2, &id_src->val, id_src->width);
-  
+  // 执行减法操作并将结果存储在t0中
   rtl_sub(&t0, &t1, &t2);
   t3 = (t0 > t1);
+  // 如果发生无符号下溢，设置进位标志（CF）
   rtl_set_CF(&t3);
+  // 如果结果的符号不正确，则设置溢出标志（OF）
   t3 = ((((int32_t)(t1) < 0) == (((int32_t)(t2) >> 31) == 0)) && (((int32_t)(t0) < 0) != ((int32_t)(t1) < 0)));
+  // 根据结果更新零标志（ZF）和符号标志（SF）
   rtl_set_OF(&t3);
   rtl_update_ZFSF(&t0, 4);
+   // 将结果写回目标操作数
   operand_write(id_dest, &t0);
   print_asm_template2(sub);
 }
