1d2024a4e8940156b578d79c30ce8821decb74a5
tracer-ics2017 <tracer@njuics.org>
2024-03-30 00:34:09 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  00:34:09 up 4 days, 21:12,  1 user,  load average: 0.00, 0.01, 0.00 688510ef5d7a32f0a4138a94e1576b4d7149136d

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,37 +7,18 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  // t2 = id_dest->val - id_src->val
-  rtl_sub(&t2, &id_dest->val, &id_src->val);
-
-  // 使用无符号比较来检查是否需要设置进位标志 (CF)
-  // 如果 id_dest->val 小于结果 t2，那么表示发生了借位
-  rtl_sltu(&t3, &id_dest->val, &t2);
-
-  // 将计算结果 t2 写回到目标操作数 id_dest 的位置
-  operand_write(id_dest, &t2);
-
-  // 更新处理器状态的零标志 (ZF) 和符号标志 (SF) 根据结果 t2 的值
-  rtl_update_ZFSF(&t2, id_dest->width);
-
-  // 使用无符号比较来检查是否需要设置进位标志 (CF)
-  rtl_sltu(&t0, &t3, &t2);
-
-  // 结合之前的进位信息来更新 CF
-  rtl_or(&t0, &t3, &t0);
-  rtl_set_CF(&t0);
-
-  // 使用异或操作来帮助计算溢出标志 (OF)
-  // 如果两个操作数的符号相同，而结果的符号与它们不同，那么表示发生了溢出
-  rtl_xor(&t0, &id_dest->val, &id_src->val); // 操作数符号是否相异
-  rtl_xor(&t1, &id_dest->val, &t2);          // 结果与被减数符号是否相异
-  rtl_and(&t0, &t0, &t1);                    // 如果操作数符号相同，结果的符号不同，则发生溢出
-  rtl_msb(&t0, &t0, id_dest->width);         // 获取结果的最高位来判断是否发生溢出
-  rtl_set_OF(&t0);                            // 设置溢出标志
-
-  print_asm("sub %%%s,%%%s", reg_name(id_dest->reg, id_dest->width), reg_name(id_src->reg, id_src->width));
+  rtl_sext(&t1, &id_dest->val, id_dest->width);
+  rtl_sext(&t2, &id_src->val, id_src->width);
+  
+  rtl_sub(&t0, &t1, &t2);
+  t3 = (t0 > t1);
+  rtl_set_CF(&t3);
+  t3 = ((((int32_t)(t1) < 0) == (((int32_t)(t2) >> 31) == 0)) && (((int32_t)(t0) < 0) != ((int32_t)(t1) < 0)));
+  rtl_set_OF(&t3);
+  rtl_update_ZFSF(&t0, 4);
+  operand_write(id_dest, &t0);
+  print_asm_template2(sub);
 }
-
 
 make_EHelper(cmp) {
   TODO();
