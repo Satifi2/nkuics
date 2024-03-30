c86c267bd118c1026050fb17fc61721c54bcb88a
tracer-ics2017 <tracer@njuics.org>
2024-03-30 00:03:03 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  00:03:03 up 4 days, 20:41,  1 user,  load average: 0.00, 0.00, 0.00 87424be7b9220faad5f22d0ef8fd84524e54ecc

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -11,10 +11,15 @@ make_EHelper(push);
 make_EHelper(pop);
 make_EHelper(sub);
 make_EHelper(ret);
+
+make_EHelper(add);
+make_EHelper(or);
+make_EHelper(adc);
+make_EHelper(sbb);
+make_EHelper(and);
+make_EHelper(sub);
 make_EHelper(xor);
-
-
-
+make_EHelper(cmp);
 
 
 
