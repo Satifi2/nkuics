01b9df0f42167cf1926ce4a72508adb20374f1fb
tracer-ics2017 <tracer@njuics.org>
2024-03-30 07:36:40 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  07:36:40 up 5 days,  3:11,  1 user,  load average: 0.03, 0.04, 0.00 423b6e44576500cbfaf35f881c4b1ad358088a3

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -227,7 +227,7 @@ void difftest_step(uint32_t eip) {
   if (diff) {
     nemu_state = NEMU_END;
   }else{
-    printf("\033[1;31m"); // 设置颜色为亮红色
+    printf("\033[1;32m"); // 设置颜色为亮绿色
     printf("nemu has no diff from qemu\n");
     printf("\033[0m"); // 重置颜色为默认
   }
