2755c44f57543c1be4278287dddf2581870c2900
tracer-ics2017 <tracer@njuics.org>
2024-03-31 06:35:26 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  06:35:26 up 5 days, 21:15,  1 user,  load average: 0.17, 0.08, 0.05 905c1a5ff83247e389ef1ac014e528356a073d88

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -19,18 +19,22 @@ make_EHelper(and) {
 
 make_EHelper(xor) {//有变动
   rtl_xor(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
-  rtl_update_ZFSF(&t2, id_dest->width);
-  operand_write(id_dest, &t2);
   print_asm_template2(xor);
 }
 
 make_EHelper(or) {
-  TODO();
-
+  rtl_or(&t0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
   print_asm_template2(or);
 }
+
 
 make_EHelper(sar) {
   TODO();
