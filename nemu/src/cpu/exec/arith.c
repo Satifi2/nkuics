fccabdeb6a13a93fb11a167ca56fc977e0bfc755
tracer-ics2017 <tracer@njuics.org>
2024-03-31 02:01:39 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  02:01:39 up 5 days, 16:41,  1 user,  load average: 0.64, 0.67, 0.65 149c7b6bd0259ccddb09c3f42d229bf4261deb

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -1,10 +1,7 @@
 #include "cpu/exec.h"
 
-make_EHelper(add) {
-  rtl_sext(&t2, &id_dest->val, id_dest->width);
-  rtl_sext(&t3, &id_src->val, id_src->width);
-
-  rtl_add(&t0, &t2, &t3);
+make_EHelper(add) {//有更改
+  rtl_add(&t0, &id_dest->val,  &id_src->val);
   t1 = (t0 < t2);
   rtl_set_CF(&t1);
   t1 = ((((int32_t)(t2) < 0) == ((int32_t)(t3) < 0)) && (((int32_t)(t0) < 0) != ((int32_t)(t2) < 0)));
