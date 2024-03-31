bcc2f545d8d4bbaadbab0b40e2243a4f400529d6
tracer-ics2017 <tracer@njuics.org>
2024-03-31 02:06:50 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  02:06:50 up 5 days, 16:46,  1 user,  load average: 0.66, 0.69, 0.66 fb162b923632b1fc83258babd4f968a194b652b1

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -1,14 +1,18 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
+  //将两个要加的数进行符号扩展
   rtl_sext(&t3, &id_src->val, id_src->width);
   rtl_sext(&t2, &id_dest->val, id_dest->width);
+  //相加
   rtl_add(&t0, &id_dest->val,  &id_src->val);
-  t1 = (t0 < t2);
-  rtl_set_CF(&t1);
-  t1 = ((((int32_t)(t2) < 0) == ((int32_t)(t3) < 0)) && (((int32_t)(t0) < 0) != ((int32_t)(t2) < 0)));
   operand_write(id_dest, &t0);
   rtl_update_ZFSF(&t0, 4);
+  //是否有进位
+  t1 = (t0 < t2);
+  rtl_set_CF(&t1);
+  //是否有溢出
+  t1 = ((((int32_t)(t2) < 0) == ((int32_t)(t3) < 0)) && (((int32_t)(t0) < 0) != ((int32_t)(t2) < 0)));
   rtl_set_OF(&t1);
   print_asm_template2(add);
 }
