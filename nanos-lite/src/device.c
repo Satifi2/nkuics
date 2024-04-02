9b101603b4fed84d17e4be0841c6da61bee06fb7
tracer-ics2017 <tracer@njuics.org>
2024-04-01 21:12:04 -0700

    >  run 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  21:12:04 up 6 days,  9:02,  1 user,  load average: 0.52, 0.15, 0.17 ae5f12ff82d520b4d3bdde852e5da50b9b0531fb

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,6 +23,5 @@ void fb_write(const void *buf, off_t offset, size_t len) {
 void init_device() {
   _ioe_init();
 
-  // FINISH: print the string to array `dispinfo` with the format
-  // described in the Navy-apps convention
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", _screen.width, _screen.height);
 }
