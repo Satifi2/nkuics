d17c194149cd6f2b55d791fb10fc3a28987540e3
tracer-ics2017 <tracer@njuics.org>
2024-04-03 04:55:25 -0700

    >  run 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  04:55:25 up 6 days, 11:24,  1 user,  load average: 0.22, 0.06, 0.02 eee1663f4768d2eb7175eded1919b4d81e4ca18c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,10 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
-uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+uintptr_t loader(_Protect* as, const char* filename) {
+  void* dest = DEFAULT_ENTRY;
+  size_t len = get_ramdisk_size();
+  ramdisk_read(dest, 0, len);
   return (uintptr_t)DEFAULT_ENTRY;
 }
+
