cf31b07cd068ce4fad348f946d1fd7c5a28261a3
tracer-ics2017 <tracer@njuics.org>
2024-04-03 04:36:53 -0700

    >  run 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  04:36:53 up 6 days, 11:06,  1 user,  load average: 0.13, 0.07, 0.02 6f001fd983c7660345191dc164ce581688dc19b5

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,6 +3,19 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  int fd = fs_open(filename, 0, 0);
+  uint32_t i = 0;
+  void *pageptr;
+  for(i = 0; i < fs_filesz(fd); i += 0x1000) {
+    pageptr = new_page();
+    _map(as, (void *)(0x8048000 + i), pageptr);
+    fs_read(fd, pageptr, ((fs_filesz(fd) - i >= 0x1000) ? 0x1000 : fs_filesz(fd) - i));
+  }
+  
+  fs_close(fd);
+
+  new_page();
+  
   return (uintptr_t)DEFAULT_ENTRY;
 }
+
