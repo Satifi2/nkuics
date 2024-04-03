9d80a969131b5edc71322a4ef9a37d3283add03f
tracer-ics2017 <tracer@njuics.org>
2024-04-03 04:44:40 -0700

    >  compile 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  04:44:40 up 6 days, 11:14,  1 user,  load average: 0.07, 0.06, 0.02 f270c59ea71b9c2a70ad3d33917e6c776e2275eb

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -5,6 +5,10 @@
 #include <klib.h>
 #include "debug.h"
 
+void ramdisk_read(void *buf, off_t offset, size_t len);
+void ramdisk_write(const void *buf, off_t offset, size_t len);
+size_t get_ramdisk_size();
+
 typedef char bool;
 #define true 1
 #define false 0
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,20 +2,10 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
-uintptr_t loader(_Protect *as, const char *filename) {
-  int fd = fs_open(filename, 0, 0);
-  uint32_t i = 0;
-  void *pageptr;
-  for(i = 0; i < fs_filesz(fd); i += 0x1000) {
-    pageptr = new_page();
-    _map(as, (void *)(0x8048000 + i), pageptr);
-    fs_read(fd, pageptr, ((fs_filesz(fd) - i >= 0x1000) ? 0x1000 : fs_filesz(fd) - i));
-  }
-  
-  fs_close(fd);
-
-  new_page();
-  
+uintptr_t loader(_Protect* as, const char* filename) {
+  void* dest = DEFAULT_ENTRY;
+  size_t len = get_ramdisk_size();
+  ramdisk_read(dest, 0, len);
   return (uintptr_t)DEFAULT_ENTRY;
 }
 
