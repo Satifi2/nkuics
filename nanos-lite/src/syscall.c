206edd888694e60c61ae80b005fc57cdd52be334
tracer-ics2017 <tracer@njuics.org>
2024-04-28 01:04:49 -0700

    >  run 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  01:04:49 up 7 days,  6:29,  1 user,  load average: 0.17, 0.13, 0.05 9a7f816413a3c303cd6728ae4d7a029d701ac0c6

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -54,7 +54,7 @@ _RegSet* do_syscall(_RegSet *r) {
         _halt(a[1]);
         break;
     case SYS_write_: 
-        // printf("sys_write\n");
+        printf("sys_write\n");
         result = sys_write(a[1], (void *)a[2], a[3]);
         break;
     case SYS_brk_:
