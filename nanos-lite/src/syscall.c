cddc469fd91ee109dc743863c05c98ae44a20b3b
tracer-ics2017 <tracer@njuics.org>
2024-04-28 01:16:15 -0700

    >  run 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  01:16:15 up 7 days,  6:40,  1 user,  load average: 0.22, 0.07, 0.03 c48f2aab91202d3d68ee26f47d2c8dc170e4e707

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -46,7 +46,7 @@ _RegSet* do_syscall(_RegSet *r) {
 
     switch (a[0]) {
     case SYS_none_:
-        printf("sys_none,%d,%d,%d\n",SYS_none_,SYS_exit_,SYS_write_);
+        Log("sys_none,%d,%d,%d,%d\n",SYS_none_,SYS_exit_,SYS_write_,SYS_brk_);
         result = 1;
         break;
     case SYS_exit_:
@@ -54,7 +54,7 @@ _RegSet* do_syscall(_RegSet *r) {
         _halt(a[1]);
         break;
     case SYS_write_: 
-        // printf("sys_write\n");
+        Log("sys_none,%d,%d,%d,%d\n",SYS_none_,SYS_exit_,SYS_write_,SYS_brk_);
         result = sys_write(a[1], (void *)a[2], a[3]);
         break;
     case SYS_brk_:
