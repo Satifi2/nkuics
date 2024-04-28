2e5f4ca0320dc0c62262f4e79411e6554ee6d1cb
tracer-ics2017 <tracer@njuics.org>
2024-04-28 06:08:37 -0700

    >  run 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  06:08:37 up 7 days, 11:13,  1 user,  load average: 0.44, 0.11, 0.03 d70cf945780fcf9f839fa12a6f7560807c0a7ef1

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -29,7 +29,7 @@ extern void fb_write(const void *buf, off_t offset, size_t len);
 extern size_t events_read(void *buf, size_t len);
 
 void init_fs() {
-  // FINISH: initialize the size of /dev/fb
+	file_table[FD_FB].size = _screen.height * _screen.width * 4;
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
