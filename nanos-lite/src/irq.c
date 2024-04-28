7336c22f310a2184b921b622a27d3d06b29d7a76
tracer-ics2017 <tracer@njuics.org>
2024-04-28 02:15:13 -0700

    >  run 2112847 dyx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  02:15:13 up 7 days,  7:39,  1 user,  load average: 0.17, 0.12, 0.06 7e956f97cf1ec85a8dd3ef52a09f3713bb508c21

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,6 +5,7 @@ extern _RegSet* schedule(_RegSet *prev);
 extern _RegSet* do_syscall(_RegSet *r);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
+  Log("do_event=%d", e.event);
   switch (e.event) {
     case _EVENT_SYSCALL: 
       return do_syscall(r);
